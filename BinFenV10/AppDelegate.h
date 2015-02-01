83d8ed4034555d4cc18ec156c62efba44d58cb48
dragonII <31319070@qq.com>
2015-02-01 21:03:15 +0800

    Initial Commit

---
+++ BinFenV10/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BinFenV10
+//
+//  Created by Wang Long on 2/1/15.
+//  Copyright (c) 2015 Wang Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
