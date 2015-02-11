32374fe2c33ddfd6cd55b3a1f105e1c7462babde
dragonII <31319070@qq.com>
2015-02-11 11:28:39 +0800

    关于主页面添加，分页面待完成

--- BinFenV10/AboutMeViewController.m
+++ BinFenV10/AboutMeViewController.m
@@ -7,8 +7,21 @@
 //
 
 #import "AboutMeViewController.h"
+#import "UserInforTableViewCell.h"
+#import "BasicSettingTableCell.h"
 
-@interface AboutMeViewController ()
+static NSString *UserInfoCellIdentifier = @"UserInfoCell";
+static NSString *BasicSettingCellIdentifier = @"BasicSettingCell";
+
+typedef enum
+{
+    SectionIndexUser = 0,
+    SectionIndexAddr,
+    SectionIndexOthers,
+    SectionIndexAbout
+} SectionIndexType;
+
+@interface AboutMeViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (strong, nonatomic) UITableView *tableView;
 
@@ -43,13 +56,15 @@
                                                      alpha:1.0f];
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     
-    /*
-    UINib *nib = [UINib nibWithNibName:@"NearbyTableViewCell" bundle:nil];
-    [self.tableView registerNib:nib forCellReuseIdentifier:NearbyCellIdentifier];
+    
+    UINib *nib = [UINib nibWithNibName:@"UserInforTableViewCell" bundle:nil];
+    [self.tableView registerNib:nib forCellReuseIdentifier:UserInfoCellIdentifier];
+    
+    nib = [UINib nibWithNibName:@"BasicSettingTableCell" bundle:nil];
+    [self.tableView registerNib:nib forCellReuseIdentifier:BasicSettingCellIdentifier];
     
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
-     */
     
     [self.view addSubview:self.tableView];
 }
@@ -67,5 +82,120 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma UITAbleView DataSouce and Delegate
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 4;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    switch (section)
+    {
+        case SectionIndexUser:
+            return 1;
+    
+        case SectionIndexAddr:
+            return 1;
+            
+        case SectionIndexOthers:
+            return 2;
+            
+        case SectionIndexAbout:
+            return 1;
+            
+        default:
+            return 0;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    int sectionIndex = indexPath.section;
+    switch (sectionIndex)
+    {
+        case SectionIndexUser:
+        {
+            UserInforTableViewCell *cell = (UserInforTableViewCell *)[tableView dequeueReusableCellWithIdentifier:UserInfoCellIdentifier];
+            if(cell == nil)
+            {
+                cell = [[UserInforTableViewCell alloc] init];
+            }
+            return cell;
+        }
+            
+        case SectionIndexAddr:
+        {
+            BasicSettingTableCell *cell = (BasicSettingTableCell *)[tableView dequeueReusableCellWithIdentifier:BasicSettingCellIdentifier];
+            if(cell == nil)
+                cell = [[BasicSettingTableCell alloc] init];
+            cell.symbolImage.image = [UIImage imageNamed:@"AddrEdit"];
+            cell.titleLabel.text = @"送货地址管理";
+            
+            return cell;
+        }
+            
+        case SectionIndexOthers:
+        {
+            BasicSettingTableCell *cell = (BasicSettingTableCell *)[tableView dequeueReusableCellWithIdentifier:BasicSettingCellIdentifier];
+            if(cell == nil)
+            {
+                cell = [[BasicSettingTableCell alloc] init];
+            }
+            if(indexPath.row == 0)
+            {
+                cell.symbolImage.image = [UIImage imageNamed:@"OrderEdit"];
+                cell.titleLabel.text = @"订单";
+            } else {
+                cell.symbolImage.image = [UIImage imageNamed:@"FavoriteEdit"];
+                cell.titleLabel.text = @"收藏";
+            }
+            return cell;
+        }
+        
+        case SectionIndexAbout:
+        {
+            BasicSettingTableCell *cell = (BasicSettingTableCell *)[tableView dequeueReusableCellWithIdentifier:BasicSettingCellIdentifier];
+            if(cell == nil)
+            {
+                cell = [[BasicSettingTableCell alloc] init];
+            }
+            cell.symbolImage.image = [UIImage imageNamed:@"About"];
+            cell.titleLabel.text = @"关于";
+            return cell;
+        }
+            
+        default:
+            return nil;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.section == SectionIndexUser)
+    {
+        return 76.0f;
+    } else {
+        return 44.0f;
+    }
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 12)];
+    view.backgroundColor = [UIColor colorWithRed:225/255.0f green:225/255.0f blue:225/255.0f alpha:1.0f];
+    return view;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 12.0f;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 
 @end
